11955a57dab06284f3a564b4a4b04a7cce5fb74e
PedroFW <0wfonsecapedro@gmail.com>
2022-05-22 11:26:31 -0300

    Utilizando o módulo unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,15 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import unittest
 
-options = Options()
-options.add_argument("-profile")
-options.add_argument("/home/pedro/snap/firefox/common/.mozilla/firefox/va7i427w.pedro")
-browser=webdriver.Firefox(options=options)
+class NewVisitorTest(unittest.TestCase):
 
-browser.get('http://localhost:8000')
+    def test_can_start_a_list_and_retrieve_it_later(self): 
+    
+        options = Options()
+        options.add_argument("-profile")
+        options.add_argument("/home/pedro/snap/firefox/common/.mozilla/firefox/va7i427w.pedro")
+        browser=webdriver.Firefox(options=options)
 
-assert 'Django' in browser.title
+        browser.get('http://localhost:8000')
 
+        # Ela nota que o título da página menciona TODO
+        assert 'To-Do' in browser.title
 
-	
+        # Ela é convidada a entrar com um item TODO imediatamente
+
+        # Ela digita "Estudar testes funcionais" em uma caixa de texto
+
+        # Quando ela aperta enter, a página atualiza, e mostra a lista
+        # "1: Estudar testes funcionais" como um item da lista TODO
+        
+        # Ainda existe uma caixa de texto convidando para adicionar outro item
+        # Ela digita: "Estudar testes de unidade"
+
+        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
+
+        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
+        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+
+        # Ela visita a URL: a sua lista TODO ainda está armazenada
+
+        # Satisfeita, ela vai dormir
+        
+        self.browser.quit()
+
+if __name__ == '__main__':
+    unittest.main()
 
