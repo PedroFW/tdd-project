ba8930357f443606576f0710493a6f6981833d6b
PedroFW <0wfonsecapedro@gmail.com>
2022-06-17 16:05:42 -0300

    Refatorando o Teste Funcional

---
+++ functional_tests/base.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+import time
+import unittest
+
+MAX_WAIT = 5
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+    def setUp(self):  
+        options = Options()
+        options.add_argument("-profile")
+        options.add_argument("/home/pedro/snap/firefox/common/.mozilla/firefox/va7i427w.pedro")
+        self.browser=webdriver.Firefox(options=options)
+    def tearDown(self):  
+        self.browser.quit()
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:  
+            try:
+                table = self.browser.find_element_by_id('id_list_table')  
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return  
+            except (AssertionError, WebDriverException) as e:  
+                if time.time() - start_time > MAX_WAIT:  
+                    raise e  
+                time.sleep(0.5)
+
